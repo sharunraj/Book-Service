0b16d480637d637c4b56fb110b6daba7b876721a
sharunraj <sharunraj39@gmail.com>
2024-10-22 10:58:29 +0530

    new

--- src/main/java/com/demo/book_service/controller/BookController.java
+++ src/main/java/com/demo/book_service/controller/BookController.java
@@ -3,12 +3,7 @@ package com.demo.book_service.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.demo.book_service.dao.BookRepository;
 import com.demo.book_service.model.Book;
@@ -33,5 +28,10 @@ public class BookController {
 	@PostMapping("/books")
 	public Book addBook(@RequestBody Book newBook) {
 		return this.bookRepo.saveAndFlush(newBook);
+	}
+
+	@DeleteMapping("/books/{bid}")
+	public  void deleteBook(@PathVariable int bid) {
+		this.bookRepo.deleteById(bid);
 	}
 }
